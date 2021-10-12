50548806a38f0de0c4e5e6525d38f09897ddb472
kim tei kyung <usek24@gmail.com>
2021-10-13 00:36:06 +0900

    key

--- ch06/06_11.py
+++ ch06/06_11.py
@@ -1,8 +1,8 @@
 import pybithumb
 import time
 
-con_key = "81dd5f25e5daa70b2fff603901d2c09c"
-sec_key = "82333efegeg9eg3e77c573weg34af17a"
+con_key = "2bdd34c82dcc9aac96ead793887db3b8"
+sec_key = "abe0ed795d11f363259f688582269d7c"
 
 bithumb = pybithumb.Bithumb(con_key, sec_key)
 
